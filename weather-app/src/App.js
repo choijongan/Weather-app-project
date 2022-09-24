f609e7aad51de66457989228123be9e2f8f2c5e6
choijongan <mmm7134@naver.com>
2022-09-24 11:00:15 +0900

    .

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -10,7 +10,7 @@ import WeatherButton from './component/WeatherButton';
 //3. 5개의 버튼이 있다.(1개는 현재위치, 4개는 다른도시)
 //4. 도시버튼을 클릭할 때 마다 도시별 날씨가 보인다.
 //5. 현재 위치 기반 날씨버튼을 클릭하면 다시 현재위치 기반으로 돌아온다.
-//6. 데이터를 들고온느 동안 로딩 스피너가 돈다
+//6. 데이터를 들고온느 동안 로딩 스피너가 돈다.
 function App() {
   const [weather,setWeather]= useState(null);
   const [city,setCity]= useState('');
